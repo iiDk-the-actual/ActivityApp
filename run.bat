9a8a6c6af8219588fd88fef40c818401adf09f08
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-06-16 03:25:36 -0400

    run2

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 @echo off
 :loop
+taskkill /F /IM ActivityApp.exe /T >nul 2>&1
+timeout /t 3 /nobreak >nul
 dotnet run --project "%~dp0ActivityApp.csproj" --configuration Release
-timeout /t 10 /nobreak >nul
+timeout /t 3 /nobreak >nul
 goto loop
